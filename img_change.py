e14d9f1731361dce057e5ca35352460f8312f955
Rina-24 <rina.k.game@gmail.com>
2023-11-26 01:10:59 +0900

    透過処理　未実装

---
+++ img_change.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+# mosaic 関数では、まず画像を縮小し、その後に cv2.INTER_NEAREST を用いて最近傍補間法で再拡大。
+# これにより、モザイク処理が行われる。alpha パラメータは縮小率を制御する。
+def mosaic(img, alpha):
+    h, w, ch = img.shape
+
+    # 画像を縮小
+    img = cv2.resize(img,(int(w*alpha), int(h*alpha)))
+
+    # 縮小した画像を再拡大（モザイク処理）
+    img = cv2.resize(img,(w, h), interpolation=cv2.INTER_NEAREST)
+
+    return img
+
+# decreaseColor関数:画像の各ピクセルの色を特定の範囲ごとに変更して減色処理を行う。各色の範囲は32ごとに設定されている。
+# 例えば、0から32の範囲の色は16に、32から64の範囲の色は48に変更されます。
+def decreaseColor(img):
+    # dst:Destination（宛先）の略。処理の結果を格納
+    dst = img.copy()
+
+    # 各色の範囲ごとに値を変更
+    for i in range(1, 8):
+        idx = np.where((32 * i <= img) & (32 * (i + 1) > img))
+        dst[idx] = 32 * i + 16
+    
+    # 以下の計算と同意
+        # idx = np.where((0<=img) & (32>img))
+        # dst[idx] = 16
+        # idx = np.where((32<=img) & (64>img))
+        # dst[idx] = 48
+        # idx = np.where((64<=img) & (96>img))
+        # dst[idx] = 80
+        # idx = np.where((96<=img) & (128>img))
+        # dst[idx] = 112
+        # idx = np.where((128<=img) & (160>img))
+        # dst[idx] = 144
+        # idx = np.where((160<=img) & (192>img))
+        # dst[idx] = 176
+        # idx = np.where((192<=img) & (224>img))
+        # dst[idx] = 208
+        # idx = np.where((224<=img) & (256>=img))
+        # dst[idx] = 240
+
+    return dst
+
+# 画像のパスを絶対パスで指定する
+img = cv2.imread("img/himawari.jpg")
+
+img2 = mosaic(img, 0.2)
+
+img3 = decreaseColor(img2)
+ 
+output_path = "img/himawari_changepx.jpg"
+cv2.imwrite(output_path, img3)
---
+++ make_transparent.py
@@ -0,0 +1,116 @@
+
+# from flask import Flask, render_template, request
+# import cv2
+# import numpy as np
+
+# app = Flask(__name__)
+
+# def make_transparent(img, target_color):
+#     # BGR形式でのターゲット色
+#     target_color_bgr = np.array(target_color[::-1])
+
+#     # # ターゲット色以外のピクセルを抽出
+#     # mask = np.all(img[:, :, :3] == target_color_bgr, axis=-1)
+
+#     # # ターゲット色と一致する場合はそのまま返す
+#     # if np.all(target_color_bgr == np.array([255, 255, 255])):
+#     #     return img
+
+#     # Point 1: 白色部分に対応するマスク画像を生成
+#     mask = np.all(img[:,:,:] == [255, 255, 255], axis=-1)
+#     # Point 2: 元画像をBGR形式からBGRA形式に変換
+#     al = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+
+#     # Point3: マスク画像をもとに、白色部分を透明化
+#     dst[mask,3] = 0
+
+#     # 透過チャンネルを作成
+#     alpha_channel = np.ones_like(mask, dtype=np.uint8) * 255
+#     alpha_channel[mask] = 0
+
+#     print("mask shape:", mask.shape)
+#     print("alpha_channel shape:", alpha_channel.shape)
+
+#     # アルファチャンネルを追加
+#     img_with_alpha = np.dstack([img, alpha_channel])
+
+#     print("img_with_alpha shape:", img_with_alpha.shape)
+
+#     return img_with_alpha
+
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     # フォームから送信されたファイルを取得
+#     uploaded_file = request.files['file']
+
+#     # ユーザーが指定したピクセルサイズを取得
+#     pixel_size = int(request.form['pixel_size'])
+
+#     # ユーザーが選択したファイルフォーマットを取得
+#     file_format = request.form['file_format']
+
+#     # 画像を透過させる処理（PNGの場合のみ）
+#     if file_format == 'png':
+#         img = cv2.imread(uploaded_file.filename, cv2.IMREAD_UNCHANGED)
+
+#         # 透過させる処理（make_transparent関数を利用）
+#         img_with_alpha = make_transparent(img, [255, 255, 255])
+
+#         # 画像保存
+#         cv2.imwrite("output.png", img_with_alpha)
+
+#     # その他の処理（JPGの場合など）
+
+#     return render_template('result.html', result_message='変換が完了しました。')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask, render_template, request
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+def make_transparent(img, target_color):
+    # BGR形式でのターゲット色
+    target_color_bgr = np.array(target_color[::-1])
+
+    # 白色部分に対応するマスク画像を生成
+    mask = np.all(img[:, :, :3] == target_color_bgr, axis=-1)
+
+    # 元画像をBGR形式からBGRA形式に変換
+    img_with_alpha = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+
+    # マスク画像をもとに、白色部分を透明化
+    img_with_alpha[mask, 3] = 0
+
+    return img_with_alpha
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # フォームから送信されたファイルを取得
+    uploaded_file = request.files['file']
+
+    # ユーザーが指定したピクセルサイズを取得
+    pixel_size = int(request.form['pixel_size'])
+
+    # ユーザーが選択したファイルフォーマットを取得
+    file_format = request.form['file_format']
+
+    # 画像を透過させる処理（PNGの場合のみ）
+    if file_format == 'png':
+        img = cv2.imread(uploaded_file.filename, cv2.IMREAD_UNCHANGED)
+
+        # 透過させる処理（make_transparent関数を利用）
+        img_with_alpha = make_transparent(img, [255, 255, 255])
+
+        # 画像保存
+        cv2.imwrite("output.png", img_with_alpha)
+
+    # その他の処理（JPGの場合など）
+
+    return render_template('result.html', result_message='変換が完了しました。')
+
+if __name__ == '__main__':
+    app.run(debug=True)
