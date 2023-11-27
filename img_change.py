a01a237326c96fe2e8c3aeb1d7c42b32c38e554b
Rina-24 <rina.k.game@gmail.com>
2023-11-27 23:22:34 +0900

    説明追加

--- img_change.py
+++ img_change.py
@@ -3,6 +3,10 @@ import numpy as np
  # mosaic 関数では、まず画像を縮小し、その後に cv2.INTER_NEAREST を用いて最近傍補間法で再拡大。
  # これにより、モザイク処理が行われる。alpha パラメータは縮小率を制御する。
 def mosaic(img, alpha):
+    """ img.shape: これはNumPy配列の形状を示す属性。
+    通常、画像データは高さ x 幅 x チャンネル数（もしカラー画像ならばRGBやBGRAの3または4）の形状を持つ。
+    h, w, ch = img.shape: これは複数の変数に同時に代入を行う構文で、img.shape の返り値を高さ h、幅 w、およびチャンネル数 ch にそれぞれ代入。
+    したがって、この行の結果として、変数 h には画像の高さ、w には画像の幅、ch には画像のチャンネル数が代入"""
     h, w, ch = img.shape
     
     # 画像を縮小
